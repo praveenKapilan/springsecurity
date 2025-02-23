1e482f565084ddd56f5a9a471f2cc801d62e7777
Praveen <praveenkapilan96@gmail.com>
2025-02-24 01:37:44 +0530

    created new dev branch and adding security files

---
+++ security/security/src/main/java/com/alibou/security/auth/AuthenticationResponse.java
@@ -0,0 +1,20 @@
+package com.alibou.security.auth;
+
+import lombok.*;
+
+public class AuthenticationResponse {
+    private String token;
+
+    public AuthenticationResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
+
